8a51a5ad3ac34d0bd937b6df5b55eae5df8c7345
sweeter46 <okkesyaglici46@gmail.com>
2026-06-27 19:18:51 +0300

    start methodu guncellendi

--- main.py
+++ main.py
@@ -47,3 +47,10 @@ async def analyze(incident: Incident):
     }
     
     return brief
+
+import uvicorn
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
